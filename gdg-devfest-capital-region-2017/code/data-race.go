bb2f7aaeee3b2d172517ce812b8bbf98bb64b1e8
Blain Smith <rebelgeek@blainsmith.com>
2017-10-26 19:51:04 -0400

    better mutex logging

--- gdg-devfest-capital-region-2017/code/data-race.go
+++ gdg-devfest-capital-region-2017/code/data-race.go
@@ -9,20 +9,19 @@ import (
 type counter int
 
 func (c *counter) inc() {
-	*c++                             // Write data race
-	fmt.Println("current value", *c) // Read data race
+	fmt.Println("pre inc", *c)
+	*c++
+	fmt.Println("post inc", *c)
+	fmt.Println("")
 }
 
 func main() {
 	c := counter(0)
-	// 3 goroutines reading/writing c
 	go func() { c.inc() }()
 	go func() { c.inc() }()
 	go func() { c.inc() }()
 
 	time.Sleep(1 * time.Second)
-
-	fmt.Println("final value", c)
 }
 
 // END data-race OMIT
--- gdg-devfest-capital-region-2017/code/mutex.go
+++ gdg-devfest-capital-region-2017/code/mutex.go
@@ -12,24 +12,21 @@ var mu sync.RWMutex
 type counter int
 
 func (c *counter) inc() {
-	mu.Lock()                        // Lock the mutex from others
-	*c++                             // Increment c
-	fmt.Println("current value", *c) // Print c
-	mu.Unlock()                      // Unlock mutex so others can read/write
+	mu.Lock() // Lock the mutex from others
+	fmt.Println("pre inc", *c)
+	*c++
+	fmt.Println("post inc", *c)
+	fmt.Println("")
+	mu.Unlock() // Unlock mutex so others can read/write
 }
 
 func main() {
 	c := counter(0)
-	// 3 goroutines reading/writing c
 	go func() { c.inc() }()
 	go func() { c.inc() }()
 	go func() { c.inc() }()
 
 	time.Sleep(1 * time.Second)
-
-	mu.RLock()
-	fmt.Println("final value", c)
-	mu.RUnlock()
 }
 
 // END mutex OMIT
